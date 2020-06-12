89c6d9d7450edb1a5ea84a16e8250de8a005089f
miadoherty <51769868+miadoherty@users.noreply.github.com>
2020-06-12 14:56:58 +1200

    Add files via upload

--- 11th June.py
+++ 11th June.py
@@ -28,17 +28,37 @@ def end():
         end()
 
 
-# function prints instructions
+# function greets user
 def hello():
     choice = str(input("Howdy partner! Is this your first rodeo? ")).lower()
     if choice == "yes":
-        print("intro plays here. ")
+        instruct()
     elif choice == "no":
-        print("Alrighty. ")
+        print("Intro plays here. ")
     else:
         error()
         hello()
 
 
+# function prints instructions
 def instruct():
-    print("It's pretty simple. Answer the questions with the corresponding lettter. ")
+    print("It's pretty simple. Answer the questions with the corresponding letter. ")
+    print("Type end game at any time to, you'll never guess, end the game. ")
+    choice = str(input("Do you understand? ")).lower()
+    if choice == "yes":
+        print("intro plays here. ")
+    elif choice == "no":
+        print("Alright... One more time. ")
+        instruct()
+    else:
+        error()
+        instruct()
+
+
+# tracks number of trees in rainforest????????????????????????????
+while rainforest < 100:
+    print("You need to plant more trees. ")
+    ra = raw_input("How many trees do you want to plant? ")
+
+
+hello()
