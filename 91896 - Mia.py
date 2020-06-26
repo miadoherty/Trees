64bda2249476a9b9ecfdb347f0cc7452f9f1cc28
miadoherty <51769868+miadoherty@users.noreply.github.com>
2020-06-26 14:59:40 +1200

    Add files via upload

---
+++ 91896 - Mia.py
@@ -0,0 +1,108 @@
+import time
+
+valid_answers = ["a", "b", "c"]
+
+
+# function checks answers and makes sure they are valid ^
+def checker(choice):
+    if choice in valid_answers:
+        return True
+    else:
+        print("Invalid choice, try again: ")
+        return False
+
+
+# function to display error message
+def error():
+    print("Invalid answer, please try again. ")
+
+
+# function to end game at any time
+def exit():
+    value = " "
+    choice = str(input("Are you sure you want to end the game? ")).lower()
+    if choice == "yes":
+        value = "Y"
+    elif choice == "no":
+        value = "N"
+        print("Alright let's get right back to it. ")
+    else:
+        error()
+        exit()
+    return value
+
+
+# function reads goodbye message to user
+def quit():
+    print("Thanks for playing :) ")
+
+
+# function greets user
+def hello():
+    choice = str(input("Howdy partner! Is this your first rodeo? ")).lower()
+    if choice == "yes":
+        instruct()
+    elif choice == "no":
+        intro()
+    elif choice == "end game":
+        x = exit()
+        if x == "Y":
+            quit()
+        else:
+            hello()
+    else:
+        error()
+        hello()
+
+
+# function prints instructions
+def instruct():
+    print("It's pretty simple. ")
+    print("Answer the questions with the corresponding letter. ")
+    print("Type end game at any time to, you'll never guess, end the game. ")
+    choice = str(input("Do you understand? ")).lower()
+    if choice == "yes":
+        intro()
+    elif choice == "no":
+        print("Alright... One more time. ")
+        instruct()
+    elif choice == "end game":
+        x = exit()
+        if x == "Y":
+            quit()
+        else:
+            instruct()
+    else:
+        error()
+        instruct()
+
+
+# function prints the introduction
+def intro():
+    print("Greetings weary traveller. ")
+    print("It may or may not come as a surprise to you that... ")
+    print("the world has been ruined. ")
+    print("For undisclosed reasons, it is now your "
+          "responsibility to revive earth to its former glory. ")
+    print("You are going to travel through the various biomes of Earth, ")
+    print("making sure to plant the right vegetation "
+          "in the appropriate places. ")
+    print("This is a game of choices. ")
+    print("Just be sure they are the right ones. ")
+    choice = str(input("Are you ready to play? ")).lower()
+    if choice == "yes":
+        print("The game starts now. ")
+    elif choice == "no":
+        print("The game does not start now. ")
+    elif choice == "end game":
+        x = exit()
+        if x == "Y":
+            quit()
+        else:
+            intro()
+    else:
+        error()
+        intro()
+
+
+hello()
