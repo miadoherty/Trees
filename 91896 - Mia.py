174d29c872ff4ea0721deae3695e9cb52fe343d2
miadoherty <51769868+miadoherty@users.noreply.github.com>
2020-07-01 12:09:38 +1200

    Add files via upload

--- 91896 - Mia.py
+++ 91896 - Mia.py
@@ -80,15 +80,17 @@ def instruct():
 # function prints the introduction
 def intro():
     print("Greetings weary traveller. ")
+    time.sleep(1.5)
     print("It may or may not come as a surprise to you that... ")
+    time.sleep(1)
     print("the world has been ruined. ")
+    time.sleep(1)
     print("For undisclosed reasons, it is now your "
           "responsibility to revive earth to its former glory. ")
+    time.sleep(2)
     print("You are going to travel through the various biomes of Earth, ")
     print("making sure to plant the right vegetation "
           "in the appropriate places. ")
-    print("This is a game of choices. ")
-    print("Just be sure they are the right ones. ")
     choice = str(input("Are you ready to play? ")).lower()
     if choice == "yes":
         print("The game starts now. ")
