39e52fe4ae891c6572893751e040505d67d6445a
miadoherty <51769868+miadoherty@users.noreply.github.com>
2020-07-29 12:09:03 +1200

    Add files via upload

--- 91896 - Mia.py
+++ 91896 - Mia.py
@@ -4,6 +4,7 @@ import math
 
 valid_answers = ["a", "b", "c"]
 
+
 # function checks answers and makes sure they are valid ^
 def checker(choice):
     if choice in valid_answers:
@@ -45,7 +46,7 @@ def hello():
         instruct()
     elif choice == "no":
         print("loading... ")
-        time.sleep(2)
+        time.sleep(3)
         intro()
     elif choice == "end game":
         x = exit()
@@ -67,6 +68,9 @@ def instruct():
     time.sleep(1.5)
     print("You will start with $1 in your pocket. ")
     time.sleep(1)
+    print("With more tree comes more resources "
+          "which in turn can be sold for money. ")
+    time.sleep(1)
     print("As you make more money, you will be able to unlock new vegetation "
           "to plant, ")
     time.sleep(1)
@@ -77,13 +81,15 @@ def instruct():
     instruct_choice()
 
 
+# takes input from user
 def instruct_choice():
     choice = str(input("Do you understand? ")).lower()
     if choice == "yes":
-        intro()
+        print("Loading game... ")
+        time.sleep(2)
     elif choice == "no":
         print("Alright... One more time. ")
-        instruct_choice()
+        instruct()
     elif choice == "end game":
         x = exit()
         if x == "Y":
@@ -117,12 +123,12 @@ def intro():
     intro_choice()
 
 
-# asks user if they are ready to play
 def intro_choice():
     choice = str(input("Are you ready to play? ")).lower()
     if choice == "yes":
-        print("The game starts now. ")
-        tundra_text()
+        print("loading... ")
+        time.sleep(3)
+        tundra_intro()
     elif choice == "no":
         print("Okay. I will ask you again in five seconds. ")
         time.sleep(5)
@@ -132,49 +138,86 @@ def intro_choice():
         if x == "Y":
             quit()
         else:
-            intro()
+            intro_choice()
     else:
         error()
-        intro()
+        intro_choice()
 
 
-def tundra_text():
+def tundra_intro():
     print("Brrr... It sure is cold in the tundra. ")
     time.sleep(1)
     print("Maybe I should have worn socks. ")
+    time.sleep(1.5)
+    print("While we are here, why don't I tell you some fun facts about the tundra. ")
+    time.sleep(1.5)
+    print("Did you know that the word 'tundra' comes from the Finnish word ' tunturia'. ")
+    time.sleep(2)
+    print("'Tunturia' means barren or treeless hill. ")
+    time.sleep(2)
+    print("Rather than big trees, the tundra has patchy vegetation "
+          "that tends to be low to the ground. ")
+    time.sleep(2)
+    print("This usually consists of grasses, mosses, lichens, sedges and small shrubs. ")
+    time.sleep(1.5)
+    print("These kinds of plants are better adapted to withstand the harsh tundra conditions. ")
+    time.sleep(3)
+    print("Hey! ")
+    time.sleep(1)
+    print("What is that over there. ")
+    time.sleep(1)
+    print("It looks like a gold coin donataion. ")
+    time.sleep(1)
+    tundra_choice()
 
 
-def tundra():
-    tundra_value = 1
-    tundra_profit = 0
-    tundra_cost = 0    
-    while tundra_profit <= 100:
-        print("Would you like to plant a tree for: ")
-        choice = (input("${:.2f} ".format(tundra_cost)))
-        if choice == "yes":
-            tundra_cost = tundra_value * 2
-            tundra_value += 1
-            tundra_profit = (tundra_value ** 2) - (tundra_cost)
-            print("You have planted", tundra_value, "trees. ")
-            print("You currently have: ")
-            print("${:.2f}".format(tundra_profit))
-            time.sleep(1)
-        else:
-            error()
-    while 100 < tundra_profit < 1000:
+def tundra_choice():
+    choice = str(input("Do you want to go and pick it up? ")).lower()
+    if choice == "yes":
+
+def undra():
+    print("Introduce the tundra ")
+    undra_value = 0
+    undra_profit = 0
+    undra_cost = 0
+    while undra_profit < 100:
+        if True:
+            print("Would you like to plant a tree for: ")
+            choice = str(input("${:.2f} ".format(undra_cost))).lower()
+            if choice == "yes":
+                undra_cost = undra_value * 2
+                undra_value += 1
+                undra_profit = (undra_value ** 2) - (undra_cost)
+                print("You have planted", undra_value, "trees. ")
+                print("You currently have: ")
+                print("${:.2f}".format(undra_profit))
+            elif choice == "end game":
+                x = exit()
+                if x == "Y":
+                    quit()
+                else:
+                    undra()
+            else:
+                print("${:.2f}".format(undra_value))
+    while undra_profit >= 100:
         if True:
             print("You have made $100. You may now plant the next plant. ")
-            choice = (input("Plant tree? "))
+            choice = str(input("Plant tree? ")).lower()
             if choice == "yes":
-                tundra_cost = tundra_value ** 2
-                tundra_value += 1
-                tundra_profit = 2 * (tundra_value ** 2) - (tundra_cost)
-                print("${:.2f}".format(tundra_profit))
-                time.sleep(1)
+                undra_value += 1
+                undra_profit = (undra_value ** 3) - (undra_value * 3)
+                print("${:.2f}".format(undra_profit))
+            elif choice == "end game":
+                x = exit()
+                if x == "Y":
+                    quit()
+                else:
+                    undra()
             else:
-                error()
-    if tundra_profit >= 1000:
-        print("You have made $1000")
-
-
+                print("No ")
+    while undra_profit == 1000:
+        if True:
+            print("You have made $1000. You may now plant the next plant. ")
+            break
+  
 hello()
